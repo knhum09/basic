d10d4928d766f802f041fc5e7360cd064e0574f3
knhum09 <n-kr-nrru@hotmail.com>
2016-10-17 14:26:59 +0700

    first commit

---
+++ oop1.php
@@ -0,0 +1,25 @@
+<?php
+class car{
+    public $color;//property
+    public function setColor($c) {
+        return 'Car is : '.$this->color=$c;
+    }
+    public function sayHello() {
+        return 'Sey Hello';
+    }
+}
+
+
+$newcar=new car;
+
+$test=$newcar->setColor('red');
+echo $test;
+echo '<hr>';
+
+echo $newcar->sayHello();
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
---
+++ testbasic1.php
@@ -0,0 +1,9 @@
+<?php
+
+$cat='red';
+if($cat=='red'){
+    echo 'cat is red';
+}else{
+    echo 'cat is not red';
+}
+?>
